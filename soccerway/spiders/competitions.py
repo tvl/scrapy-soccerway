5a7f1ec21852c792e56794211ea1bc34884dee92
Vladimir Tarasov <vt@unistra.ru>
2017-04-11 17:00:49 -0400

    Fixes

--- soccerway/spiders/competitions.py
+++ soccerway/spiders/competitions.py
@@ -25,17 +25,17 @@ class CompetitionsSpider(Spider):
 
     def parse(self, response):
         items = []
-        item = Competition()
         area_id = parse_qs(response.xpath('//div[@class="clearfix subnav level-1"]//li//a[2]/@href').extract_first())['area_id'][0]
         area_name = response.xpath('//div[@class="clearfix subnav level-1"]//li//a[2]/text()').extract_first()
         links = response.xpath('//div[@class="block_competitions_list real-content clearfix "]//li/a')
         for l in links:
+            item = Competition()
             item['id'] = parse_qs(l.xpath('./@href').extract_first())['id'][0]
             item['name'] = l.xpath('./text()').extract_first()
             item['area_id'] = area_id
             item['area_name'] = area_name
-            item['updated'] = datetime.utcnow().isoformat()
-            #yield item
+            item['updated'] = datetime.utcnow().isoformat(' ')
+            yield item
             items.append(item)
         return items
         #self.log('URL: {}'.format(response.url))

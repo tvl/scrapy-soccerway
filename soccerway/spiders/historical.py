281e7a81b49093e62df70bdcf3511ca85051e2cc
Vladimir Tarasov <vt@unistra.ru>
2017-04-18 22:28:23 +0300

    Fixes

--- soccerway/spiders/historical.py
+++ soccerway/spiders/historical.py
@@ -12,7 +12,7 @@ class HistoricalSpider(Spider):
 
     def start_requests(self):
         start_url = 'http://www.soccerway.mobi/?sport=soccer&page=match&id={}&_teamtype=default&localization_id=www'
-        for i in range(2229000, 2229600):
+        for i in range(2210100, 2210700):
             request = Request(url=start_url.format(str(i)), callback=self.parse)
             request.meta['proxy'] = 'http://127.0.0.1:8118'
             yield request
@@ -33,13 +33,13 @@ class HistoricalSpider(Spider):
         #item['competition'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Competition"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Competition"]/a/text()').extract_first()
         item['game_week'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Game week"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Game week"]/text()').extract_first()
         item['kick_off'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Kick-off"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Kick-off"]//span/text()').extract_first()
-        item['hts'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Half-time"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Half-time"]//span/text()').extract_first()
-        item['fts'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Full-time"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Full-time"]//span/text()').extract_first()
-        item['ets'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Extra-time"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Extra-time"]//span/text()').extract_first()
-        item['pts'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Penalties"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Penalties"]//span/text()').extract_first()
+        item['hts'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Half-time"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Half-time"]/text()').extract_first()
+        item['fts'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Full-time"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Full-time"]/text()').extract_first()
+        item['ets'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Extra-time"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Extra-time"]/text()').extract_first()
+        item['pts'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Penalties"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Penalties"]/text()').extract_first()
         item['score'] = response.xpath('//h3[@class="thick scoretime "]/text()').extract_first().split('\n')[2].strip()
         item['venue'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Venue"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Venue"]//a/text()').extract_first()
-        item['attendance'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Attendance"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Attendance"]//a/text()').extract_first()
+        item['attendance'] = response.xpath('//div[@class="details clearfix"]/dl/dt[.="Attendance"]/following-sibling::dd[preceding-sibling::dt[1]/text()="Attendance"]/text()').extract_first()
         item['updated'] = datetime.utcnow()
         yield item
         return item

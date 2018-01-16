8017364c320dc409ae99707233a9a435b6ef2d68
Vladimir Tarasov <vt@unistra.ru>
2018-01-16 17:33:44 +0300

    Add parse_log.py

---
+++ soccerway/parse_log.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+def venues():
+    l = []
+    df = pd.read_csv('404.log', delimiter=' ', header=None)
+
+    for row in df[7]:
+        l.append(int((row.split('/')[6][1:])))
+    print('Nonexitent venues:')
+    print(l)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    venues()
